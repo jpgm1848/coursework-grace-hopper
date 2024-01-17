3458591a1e1adafd472198ecd105348dab2513c9
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-01-17 16:29:32 -0400

    vault assignment

---
+++ block07/js-fundamentals/app.js
@@ -0,0 +1,15 @@
+alert("Have a great day!");
+
+console.log("This is a log message for developers.");
+
+//variables can change, constants cannot be changed
+
+let firstName = "Alice";
+const PI = 3.1415926;
+let isRainy = false;
+
+console.log(firstName);
+
+// template literal
+
+console.log(`Hello ${firstName}.`);
---
+++ block07/vault/vault.js
@@ -0,0 +1,21 @@
+// A string will be created with the user string.
+// The three variables will be created
+// An alert with the welcome message concatenated with the numbers will be
+
+welcome =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Each variable has a different operation
+
+let numOne = 5 + 5; //equals 10
+let numTwo = 50 - 10; // equals 40
+let numThree = 13 * 3; //equals 39
+
+//A console.log is created to verify that the message is printing out correctly.
+//It should only appear in the console and not the alert message.
+
+console.log(`${welcome} ${numOne} - ${numTwo} - ${numThree}`);
+
+// An alert message is created concatenating the message with the numbers.
+
+alert(`${welcome} ${numOne} - ${numTwo} - ${numThree}`);
