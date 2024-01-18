f6d0b80485205f421f0591dc274f1fd33396ba35
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-01-18 11:33:45 -0400

    corrected pseudocode errors

--- block07/vault/vault.js
+++ block07/vault/vault.js
@@ -1,6 +1,6 @@
 // A string will be created with the user string.
 // The three variables will be created
-// An alert with the welcome message concatenated with the numbers will be
+// An alert with the welcome message concatenated with the numbers will be created.
 
 let welcome =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
